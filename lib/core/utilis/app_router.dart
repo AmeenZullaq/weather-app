83215e3d6e36255bcea758070ce1616db03f7c98
ameen <ameenemails@gmail.com>
2023-11-07 20:07:37 +0200

    create service locator method

--- lib/core/utilis/app_router.dart
+++ lib/core/utilis/app_router.dart
@@ -1,4 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:waether_app/core/utilis/api_service.dart';
+import 'package:waether_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:waether_app/features/home/presentation/manager/current_weather_cubit/current_weather_cubit.dart';
 import 'package:waether_app/features/home/presentation/views/home_view.dart';
 
 import '../../features/home/presentation/views/forecast_view.dart';
@@ -10,7 +15,10 @@ class AppRouter {
       GoRoute(
         path: '/',
         builder: (context, state) {
-          return const HomeView();
+          return  BlocProvider(
+            create: (context) => CurrentWeatherCubit(HomeRepoImpl(ApiService(Dio()))),
+            child: HomeView(),
+          );
         },
       ),
       GoRoute(

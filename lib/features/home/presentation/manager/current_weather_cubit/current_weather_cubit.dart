09c0e57d41a54688893676789602501ec7c753ea
ameen <ameenemails@gmail.com>
2023-11-07 10:50:13 +0200

    create current weather cubit

--- lib/features/home/presentation/manager/current_weather_cubit/current_weather_cubit.dart
+++ lib/features/home/presentation/manager/current_weather_cubit/current_weather_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:waether_app/features/home/data/models/weather_model/weather_model/weather_model.dart';
+import 'package:waether_app/features/home/data/repos/home_repo.dart';
 
 part 'current_weather_state.dart';
 
 class CurrentWeatherCubit extends Cubit<CurrentWeatherState> {
-  CurrentWeatherCubit() : super(CurrentWeatherInitial());
+  CurrentWeatherCubit(this.homeRepo) : super(CurrentWeatherInitial());
+  final HomeRepo homeRepo;
+
+  Future fetchCurrentWeather({required String cityName}) async {
+    emit(CurrentWeatherLoading());
+
+    var result = await homeRepo.fetchCurrentWeather(cityName: cityName);
+    result.fold(
+      (left) {
+        emit(CurrentWeatherFailure(left.errMessage));
+      },
+      (right) {
+        emit(CurrentWeatherSuccess(right));
+      },
+    );
+  }
 }
